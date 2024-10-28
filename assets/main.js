9e01c2ead3d0edaea7721450d9906584f618613b
IB <i.balijon@gmail.com>
2024-10-28 11:14:56 +0100

    create elements camera

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+import Order from './model/Order.js';
+import Cart from './model/Cart.js';
+import Camera from './model/Camera.js';
+
--- assets/model/Camera.js
+++ assets/model/Camera.js
@@ -6,4 +6,33 @@ export default class Camera {
         this.prix = prix;
         this.image = image;
     }
+
+    createPreviewCardElement() {
+        const card = document.createElement('article');
+        card.className = 'product-card';
+
+        const img = document.createElement('img');
+        img.src = this.image;
+        img.alt = this.nom_produit;
+        card.appendChild(img);
+
+        const title = document.createElement('h3');
+        title.textContent = this.nom_produit;
+        card.appendChild(title);
+
+        const price = document.createElement('p');
+        price.className = 'price';
+        price.textContent = this.prix;
+        card.appendChild(price);
+
+        const button = document.createElement('button');
+        button.textContent = 'Add to Cart';
+        button.addEventListener('click', () => Cart.addToCart(this));
+        card.appendChild(button);
+
+        return card;
+    }
+    createDetailedCardElement() {
+        //TODO
+    }
 }
